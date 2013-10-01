9efdbbb251880be2d68e5fc0443990d0ea6130ed
Cédric Messiant <cedricmessiant@ecreall.com>
2013-10-01 16:59:56 +0200

    Add some params and enhance main.cf template

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,8 +1,16 @@
 class postfix::params {
   case $::lsbdistcodename {
-    'squeeze': {
-      $email = hiera('email')
-      $host  = hiera('host')
+    'squeeze' {
+      $additional_destinations  = hiera('additional_destinations', '')
+      $aliases = hiera_hash('aliases')
+      $relayhost  = hiera('relayhost', '')
+      $smtpd_recipient_restrictions = hiera('smtpd_recipient_restrictions', 'permit_mynetworks, reject_unauth_destination')
+    }
+    'wheezy': {
+      $additional_destinations  = hiera('additional_destinations', '')
+      $aliases = hiera_hash('aliases')
+      $relayhost  = hiera('relayhost', '')
+      $smtpd_recipient_restrictions = hiera('smtpd_recipient_restrictions', 'permit_mynetworks, reject_unauth_destination')
     }
     default: {
       fail("Module ${module_name} does not support ${::lsbdistcodename}")
